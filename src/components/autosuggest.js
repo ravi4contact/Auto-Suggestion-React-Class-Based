ce735ff0f1ced15a02fc409fc3c043609725eadf
ravi4contact <ravi4contact@gmail.com>
2020-08-12 03:58:26 +0530

    Add files via upload

--- src/components/autosuggest.js
+++ src/components/autosuggest.js
@@ -1,6 +1,7 @@
-// basic.autosuggest.js
+
 import React, { Fragment } from 'react';
 import Autosuggest from 'react-autosuggest';
+import IsolatedScroll from 'react-isolated-scroll';
 import AutosuggestHighlightMatch from 'autosuggest-highlight/match'
 import AutosuggestHighlightParse from 'autosuggest-highlight/parse'
 import './autosuggest.css'; 
@@ -9,16 +10,14 @@ import Clear from "./images/clear.png";
 import { people } from "./db/data";
 import { getUsers, debouncedSearchAPI,throttledSearchAPI } from "./apis/user";
 
-
-
 class FocusAutoSuggest extends React.Component {
     constructor() {
         super();
-
-        //Define state for value and suggestion collection
         this.state = {
             value: '',
-            suggestions: []
+            suggestions: [],
+            refInc: 0,
+            currentAction: 'type'
         };
     }
     escapeRegexCharacters = (str)  => {
@@ -52,70 +51,84 @@ class FocusAutoSuggest extends React.Component {
         return suggestionFilter;
     }
 
-    // Trigger suggestions
+    
     getSuggestionValue = suggestion => suggestion.name;
 
-    // Render Each Option
-
-     renderSuggestion =  (suggestion, { query }) => {
-        if(suggestion === "No user found" || suggestion === "Type minimum 3 letter"){
-            return (
-                <div className ={'errorMsg'}>{suggestion}</div>
-            )
-        }
-        let idText = this.renderSuggestionSplit(suggestion.id, { query })
-        let nameText = this.renderSuggestionSplit(suggestion.name, { query })
-        let addressText = this.renderSuggestionSplit(suggestion.address, { query })
-        let pinCodeText = this.renderSuggestionSplit(suggestion.pincode, { query })
+    renderSuggestion =  (suggestion, { query }) => {
        
+    if(suggestion === "No user found" || suggestion === "Type minimum 3 letter"){
         return (
-               <div className={'suggestion-content-parent'}>
-                  <div><b>{idText}</b></div>
-                  <div><i>{nameText}</i></div>
-                  <div>{addressText}</div>
-                  <div>{pinCodeText}</div>
-               </div>)
-      }
+            <div className ={'errorMsg'}>{suggestion}</div>
+        )
+    }
+    let idText = this.renderSuggestionSplit(suggestion.id, { query })
+    let nameText = this.renderSuggestionSplit(suggestion.name, { query })
+    let itemText = this.renderSuggestionSplit(JSON.stringify(suggestion.items), { query })
+    let addressText = this.renderSuggestionSplit(suggestion.address, { query })
+    let pinCodeText = this.renderSuggestionSplit(suggestion.pincode, { query })
+    
+    if(this.state.currentAction == 'type'){
+        suggestion['uniqueId'] = suggestion.id+"-"+String(this.state.refInc++);
+       // this[suggestion['uniqueId']]= React.createRef();
+    }
+    
+    return (
+            <div  className= {`suggestion-content-parent ${suggestion.uniqueId}`}>
+                <div><b>{idText}</b></div>
+                <div><i>{nameText}</i></div>
+                <div><i>{itemText}</i></div>
+                <div>{addressText}</div>
+                <div>{pinCodeText}</div>
+            </div>)
+    }
 
-      renderSuggestionSplit =  (val, { query }) => {
-        const suggestionText = `${val}`;
-        const matches = AutosuggestHighlightMatch(suggestionText, query);
-        const parts = AutosuggestHighlightParse(suggestionText, matches);
-      
-        return (
-                <span className={'suggestion-content '}>
-                    <span className="name">
-                    {
-                        parts.map((part, index) => {
-                        const className = part.highlight ? 'highlight' : null;
-            
-                        return (
-                            <span className={className} key={index}>{part.text}</span>
-                        );
-                        })
-                    }
-                    </span>
-                </span>
-        );
-      }
-
-
-    // OnChange event handler
-    onChange = (event, { newValue }) => {
+    renderSuggestionSplit =  (val, { query }) => {
+    const suggestionText = `${val}`;
+    const matches = AutosuggestHighlightMatch(suggestionText, query);
+    const parts = AutosuggestHighlightParse(suggestionText, matches);
+    
+    return (
+            <span className={'suggestion-content '}>
+                <span className="name">
+                {
+                    parts.map((part, index) => {
+                    const className = part.highlight ? 'highlight' : null;
         
+                    return (
+                        <span className={className} key={index}>{part.text}</span>
+                    );
+                    })
+                }
+                </span>
+            </span>
+    );
+    }
+
+
+    onChange = (event, { newValue, method }) => {
+        if (method == "down" || method == "up") {
+            this.setState({
+                currentAction: 'nonType'
+            });
+            
+        }else if (method == "type") {
         this.setState({
             suggestions: [],
-            value: newValue
+            value: newValue,
+            refInc: 0,
+            currentAction: 'type'
         });
+        }
+   
     };
 
-    // Suggestion rerender when user types
     onSuggestionsFetchRequested = ({ value }) => {
         this.getSuggestions(value)
             .then(data => {
                 if (data.Error) {
                     this.setState({
-                        suggestions: []
+                        suggestions: [],
+                        refInc: 0
                     });
                 } else {
                     this.setState({
@@ -125,43 +138,51 @@ class FocusAutoSuggest extends React.Component {
             })
     };
 
-    // Triggered on clear
     onSuggestionsClearRequested = () => {
         this.setState({
-            suggestions: []
+            suggestions: [],
+            refInc: 0
         });
     };
+
     onClear = () => {
         
         this.setState({
             suggestions: [],
-            value: ''
+            value: '',
+            refInc: 0
         });
+    };
+    focusInputOnSuggestionClick = () => {
+    }
+    onSuggestionHighlighted = ({ suggestion }) =>{
+        if(suggestion){
+            let elmnt = window.document.getElementsByClassName(String(suggestion.uniqueId));
+            elmnt[0].scrollIntoView();
+        }
     };
     render() {
         const { value, suggestions } = this.state;
-
-        // Option props
         const inputProps = {
             placeholder: 'Search users by ID, address and name',
             value,
             onChange: this.onChange,
             onkeydown: this.onChange
         };
-
-        // Adding AutoSuggest component
         return (
             <Fragment>
                 <div className = {"searchLogoContainer"}>
                     <img  alt="Search" className = {"searchLogo"}src={Search}></img>
                 </div>
                 <Autosuggest
+                    
                     className = {"searchInputBox"} 
                     suggestions={suggestions}
                     onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
                     onSuggestionsClearRequested={this.onSuggestionsClearRequested}
                     getSuggestionValue={this.getSuggestionValue}
                     renderSuggestion={this.renderSuggestion}
+                    onSuggestionHighlighted={this.onSuggestionHighlighted}
                     inputProps={inputProps}
                 />
                 <div className = {"clearLogoContainer"}>
